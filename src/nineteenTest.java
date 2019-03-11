dc9a6ac3af07c6173b8a05eba0f13b25cf44b24f
Cole McCorkle <nmccorkle@elon.edu>
2019-03-11 00:14:20 -0400

    add nineteenTest

---
+++ src/nineteenTest.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class nineteenTest {
+    public static void main(String[] args) {
+
+        dataAnalyzer dA = null;
+        try {
+            dA = new dataAnalyzer("dsaStudents.txt");
+        } catch (IOException e){
+            System.out.println("Something went wrong");
+        }
+
+    }
+}
